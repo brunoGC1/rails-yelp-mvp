b7a5676ef28912773d58a70d4ae763fd1501d2e6
Bruno Carceles <bruno0092@gmail.com>
2023-01-17 22:25:46 -0300

    5 restaurants

--- Gemfile
+++ Gemfile
@@ -27,6 +27,9 @@ gem "stimulus-rails"
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 gem "jbuilder"
 
+# Simpleform
+gem "simple_form", github: "heartcombo/simple_form"
+
 # Use Redis adapter to run Action Cable in production
 # gem "redis", "~> 4.0"
 
@@ -41,6 +44,9 @@ gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
 
 # Reduces boot times through caching; required in config/boot.rb
 gem "bootsnap", require: false
+
+# Install gem faker to use
+gem "faker"
 
 # Use Sass to process CSS
 # gem "sassc-rails"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+puts "Celaning database"
+Restaurant.destroy_all
+
+puts "Creating restaurants"
+
+Cate = ["chinese", "italian", "japanese", "french", "belgian"]
+
+5.times do
+  item = Cate[rand(Cate.length)]
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_name,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: item
+  )
+  puts "Restaurant with id: #{restaurant.id} has been created"
+end
+
+puts "Finished!"
